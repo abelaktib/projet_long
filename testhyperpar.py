8fe035c1021289078c6ddcfb584d1024565db111
abelaktib <belaktib.anas@gmail.com>
2022-11-21 19:17:29 +0100

    update argparse

--- testhyperpar.py
+++ testhyperpar.py
@@ -4,9 +4,9 @@ __date__ = "16/11/2022"
 __version__ = "1.0.0"
 __copyright__ = "CC BY-SA"
 
-# IMPORTS
+# # IMPORTS
 from numba import jit
-
+import sys
 import random
 import h5py
 import os
@@ -16,6 +16,7 @@ import tensorflow_io as tfio
 import pandas as pd
 import numpy as np
 import src.cnn as cnn
+import argparse
 # import src.inception as inception
 # import src.gru as gru
 from sklearn.model_selection import GridSearchCV, cross_val_score
@@ -24,32 +25,38 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.callbacks import EarlyStopping,TensorBoard,ReduceLROnPlateau
 from sklearn.metrics import get_scorer_names
 
+parse=argparse.ArgumentParser()
+parse.add_argument("file")
+args=parse.parse_args()
+args.file
+
 
 random.seed(10)
-#
-#
-# SELECTION DU MODEL: Choisir 1 seul Modèle.
-model_cnn = cnn.cnn()
-#
-# # model_inc = inception.inception()
-#
-# # model_gru = gru.gru()
-#
-#
-###### Chargement ddes dataset ################
 
-FILE = 'data/smalldatabase_window13_c.h5'
+
+################ SELECTION DU MODEL: Choisir 1 seul Modèle.
+model_cnn = cnn.cnn()
+
+# model_inc = inception.inception()
+
+# model_gru = gru.gru()
+
+
+##### Chargement ddes dataset ################
+# argparse
+
+
 import h5py
-f = h5py.File(FILE, 'r')
+f = h5py.File(args.file, 'r')
 print(list(f.keys()))
 
 
-X_train = tfio.IODataset.from_hdf5(FILE, dataset=f"/X_training")
-Y_train = tfio.IODataset.from_hdf5(FILE, dataset=f"/Y_training")
+X_train = tfio.IODataset.from_hdf5(args.file, dataset=f"/X_training")
+Y_train = tfio.IODataset.from_hdf5(args.file, dataset=f"/Y_training")
 
 # #
-X_val = tfio.IODataset.from_hdf5(FILE, dataset=f"/X_validation")
-Y_val = tfio.IODataset.from_hdf5(FILE, dataset=f"/Y_validation")
+X_val = tfio.IODataset.from_hdf5(args.file, dataset=f"/X_validation")
+Y_val = tfio.IODataset.from_hdf5(args.file, dataset=f"/Y_validation")
 # #
 print(X_train)
 print(Y_train)
@@ -57,8 +64,8 @@ print(Y_train)
 print(X_val)
 print(Y_val)
 # ###### Ajout des poids #####
-sw_training = tfio.IODataset.from_hdf5(FILE, dataset="/sw_training")
-sw_validation = tfio.IODataset.from_hdf5(FILE, dataset="/sw_validation")
+sw_training = tfio.IODataset.from_hdf5(args.file, dataset="/sw_training")
+sw_validation = tfio.IODataset.from_hdf5(args.file, dataset="/sw_validation")
 
 # # Creation des dataset contenant les X , Y et poids  de chaque groupe
 learn = tf.data.Dataset.zip((X_train, Y_train,sw_training)).batch(64).prefetch(tf.data.experimental.AUTOTUNE)
